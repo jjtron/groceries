2cd5db5341a5e4437c76cf605fbd1cc26a034d21
Jerry Petron <gpetron4@cfl.rr.com>
2017-03-13 21:26:20 -0400

    Initial commit

---
+++ app/groceries/shared/grocery.model.ts
@@ -0,0 +1,9 @@
+export class Grocery {
+  constructor(
+    public id: string,
+    public name: string,
+    public done: boolean,
+    public deleted: boolean,
+    public getToday: boolean
+  ) {}
+}

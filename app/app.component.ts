621b2d87116f99721b8d5f739944a009587260b9
Jerry Petron <gpetron4@cfl.rr.com>
2017-04-09 16:51:25 -0400

    Prevent landscape orientation

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,16 @@
+/// <reference path="../node_modules/tns-platform-declarations/android.d.ts" />
 import { Component } from "@angular/core";
+import app = require("application");
 
 @Component({
   selector: "gr-main",
   template: "<page-router-outlet></page-router-outlet>"
 })
-export class AppComponent { }
+export class AppComponent {
+
+    constructor () {
+        var androidApp = app.android;
+        let activity = androidApp.startActivity;
+        activity.setRequestedOrientation(android.content.pm.ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+    }
+}
